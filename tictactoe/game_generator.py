a570b81d7ef0704a351811955515ae4a6121acff
MehtaSahil <sahilmehta17@gmail.com>
2016-07-21 13:04:06 -0500

    encapuslated state generation in class GameGenerator

--- tictactoe/game_generator.py
+++ tictactoe/game_generator.py
@@ -1,27 +1,44 @@
 from board import Board
 import collections
 
-combinations = collections.deque()
-def get_perms(board, player):
-	limit = len(board)
-	for i in range(0, limit):
-		if board[i] == 1 or board[i] == 0:
-			continue;
+class GameGenerator():
 
-		board[i] = player;
-		combinations.appendleft(board)
-		print str(board) + " " + str(Board(board).value)
-		if player == 1:
-			get_perms(board, 0)
-		elif player == 0:
-			get_perms(board, 1)
+	"""
+	For tree generation, define a perms method that returns
+		a list of lists of different IN ORDER permuations
+		e.g. perms([-1, -1, -1], 1) returns:
+			[1, -1, -1]
+			[-1, 1, -1]
+			[-1, -1, 1]
+		keep track of whose turn it is in the main loop
+			the player will switch when the list of lists
+			has no more states left to process
+	"""
 
-		board[i] = -1
+	def get_perms(self, board, player):
+		limit = len(board)
+		for i in range(0, limit):
+			if board[i] == 1 or board[i] == 0:
+				continue;
 
-perm_list = list()
-list_size = 9
-for i in range(0, list_size):
-	perm_list.append(-1)
+			board[i] = player;
+			# print str(board) + " " + str(Board(board).value)
+			print board
+			if player == 1:
+				self.get_perms(board, 0)
+			elif player == 0:
+				self.get_perms(board, 1)
 
-get_perms(perm_list, 1)
-print len(combinations)
+			board[i] = -1
+
+def Main():
+	perm_list = list();
+	for i in range(0, 3):
+		perm_list.append(-1)
+
+	generator = GameGenerator()
+
+	generator.get_perms(perm_list, 1)
+
+if __name__ == "__main__":
+	Main()
