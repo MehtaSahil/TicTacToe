24f8aada277f258c2dd06eee5f6d5a5a99ad7600
MehtaSahil <sahilmehta17@gmail.com>
2016-07-21 14:13:59 -0500

    completed inorder gamestate generation

--- tictactoe/game_generator.py
+++ tictactoe/game_generator.py
@@ -1,5 +1,6 @@
 from board import Board
 import collections
+import copy
 
 class GameGenerator():
 
@@ -13,16 +14,21 @@ class GameGenerator():
 		keep track of whose turn it is in the main loop
 			the player will switch when the list of lists
 			has no more states left to process
+
+	BUG : gen_inorder_primes switches player each time it processes
+		an item from the queue
+
+		It should only change player when it has processed a layer
+			of the tree
 	"""
 
-	def get_perms(self, board, player):
+	def get_all_perms(self, board, player):
 		limit = len(board)
 		for i in range(0, limit):
 			if board[i] == 1 or board[i] == 0:
 				continue;
 
 			board[i] = player;
-			# print str(board) + " " + str(Board(board).value)
 			print board
 			if player == 1:
 				self.get_perms(board, 0)
@@ -31,14 +37,75 @@ class GameGenerator():
 
 			board[i] = -1
 
+	def simple_perms(self, board, player):
+		# list of lists
+		boards = list()
+		limit = len(board)
+		for i in range(0, limit):
+			if board[i] == 1 or board[i] == 0:
+				continue
+
+			board[i] = player
+			boards.append(copy.deepcopy(board))
+			board[i] = -1
+
+		return boards
+
+	def get_inorder_perms(self, board, player):
+		q = collections.deque()
+		q.appendleft(board)
+
+		first_player = player
+		next_boards = self.simple_perms(q.pop(), first_player)
+		for l in next_boards:
+			q.appendleft(l)
+
+		if first_player == 1:
+			first_player = 0
+		else:
+			first_player = 1
+
+		node_process_limit = len(board)
+		process_level = len(board)
+		nodes_processed = 0
+
+		while q:
+			current = q.pop()
+			print current
+
+			next_boards = self.simple_perms(current, first_player)
+			for l in next_boards:
+				q.appendleft(l)
+
+			# only change player after exploring
+			# ALL movese for that player
+			nodes_processed += 1
+			if nodes_processed == node_process_limit:
+				if first_player == 1:
+					first_player = 0
+				else:
+					first_player = 1
+
+				nodes_processed = 0
+				node_process_limit *= process_level - 1
+				process_level -= 1
+				print
+
+	def factorial(self, n):
+		to_return = n
+		for i in range(1, n):
+			to_return *= i
+
+		return to_return
+
 def Main():
+	q = collections.deque()
 	perm_list = list();
 	for i in range(0, 3):
 		perm_list.append(-1)
 
-	generator = GameGenerator()
-
-	generator.get_perms(perm_list, 1)
+	gen = GameGenerator()
+	gen.get_inorder_perms(perm_list, 1)
 
 if __name__ == "__main__":
 	Main()
