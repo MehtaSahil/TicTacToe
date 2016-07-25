499571cd135615a9eee488ddcd3005b85f0d3c2d
MehtaSahil <sahilmehta17@gmail.com>
2016-07-25 16:42:11 -0500

    added prop_values to construct functional mc tree

--- tictactoe/mc_tree.py
+++ tictactoe/mc_tree.py
@@ -55,24 +55,45 @@ class Tree:
 
 		return root
 
+	def prop_vals(self, root):
+		sum = 0
+
+		if root.children == None:
+			sum += root.value
+		else:
+			for c in root.children:
+				sum += self.prop_vals(c)
+
+		root.value = sum
+		return sum
+		
+
 	def print_tree(self):
 		q = collections.deque()
 		q.appendleft(self.root)
 
 		while q:
 			current = q.pop()
+
+			# if at a leaf node, print and move on
 			if current.children == None:
+				self.print_node(current)
 				continue
 
 			for c in current.children:
-				print c.board
+				self.print_node(c)
 				q.appendleft(c)
+
+	def print_node(self, node):
+		print str(node.board) + " : " + str(node.value)
 
 def Main():
 	perm_list = [-1, -1, -1, -1, -1, -1, -1, -1, -1]
 	mc_tree = Tree(perm_list, 1)
 
-	mc_tree.print_tree();
+	# propogate values upward
+	mc_tree.prop_vals(mc_tree.root)
+	mc_tree.print_tree()
 
 if __name__ == "__main__":
 	Main()
