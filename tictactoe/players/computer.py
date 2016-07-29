77981bfb3e8dd329fdc7401fe3d62fd1d5ae5ad3
MehtaSahil <sahilmehta17@gmail.com>
2016-07-29 13:36:34 -0500

    trap detection now values opportunities to win the game rather than avoiding situations outright

--- tictactoe/players/computer.py
+++ tictactoe/players/computer.py
@@ -12,10 +12,11 @@ class ComputerPlayer:
 		"""
 
 		# If an immediate win is possible, make that move
-		for c in gamestate.children:
-			if c.is_win_for(self.computer_player):
-				gamestate = c
-				return gamestate
+		if self.can_immediately_win(gamestate):
+			for c in gamestate.children:
+				if c.is_win_for(self.computer_player):
+					gamestate = c
+					return gamestate
 
 		# If the comp is trapped, the game is over
 		non_immediate_losses = self.get_nonlosing_moves(gamestate)
@@ -36,12 +37,33 @@ class ComputerPlayer:
 			max_value = self.max_value(non_immediate_losses)
 			return self.max_move(non_immediate_losses, max_value)
 
+		for m in non_immediate_losses:
+			print "nim : %s" % str(m.board)
+
 		# Choose the best path that is not a setup
+		"""
+		TODO : two_step_setup needs to check for immediate wins!
+			right now it returns True if there is ANY possibility
+			of a trap, but does not know that in the process of
+			setting up a trap the human may have set up a win for
+			the comp (separate is_immediate_win method)
+			Make this decision less extreme in avoiding loss
+		"""
 		valid_moves = [x for x in non_immediate_losses
 			       if not self.two_step_setup(x)]
 
+		for m in valid_moves:
+			print "vm : %s" % str(m.board)
+
 		max_value = self.max_value(valid_moves)
 		return self.max_move(valid_moves, max_value)
+
+	def can_immediately_win(self, gamestate):
+		for c in gamestate.children:
+			if c.is_win_for(self.computer_player):
+				return True
+
+		return False
 
 	def max_move(self, tosearch, max_value):
 		for m in tosearch:
@@ -65,11 +87,24 @@ class ComputerPlayer:
 		return nonlosing_moves
 
 	def two_step_setup(self, compmove):
+		win_trap_balance = 0
+
+		# value loss avoidance over following a win
+		win_bias = 1
+		loss_bias = 5
+
 		for hc in compmove.children:
+			if self.can_immediately_win(hc):
+				win_trap_balance += win_bias
+				continue
+
 			# if there are no moves the computer can make
 			non_immediate_losses = self.get_nonlosing_moves(hc)
 			if len(non_immediate_losses) == 0:
-				return True
+				win_trap_balance -= loss_bias
+
+		if win_trap_balance < 0:
+			return True
 
 		return False
 
